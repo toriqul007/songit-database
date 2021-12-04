2a5972973b1b5803f0fb1a29d4def8a486722932
shopnobaz <vision2025bd@yahoo.com>
2021-12-04 14:52:58 +0100

    j html,css and python

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Create readme file
 
 Created new branch part2
+
+j changed py and html and css files
--- part2.py
+++ part2.py
@@ -1,46 +1,82 @@
 from database import query
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, request, redirect
 
-app = Flask(__name__, static_folder='static', static_url_path='')
+app = Flask(__name__)
 
-# artists = query('''
-# SELECT * FROM artists
-# ''')
 
 artists = query('''
-SELECT ar.id, ar.name, ar.description, ar.thumbnail, 
-GROUP_CONCAT(al.title, al.thumbnail)
-FROM artists AS ar
-JOIN albums AS al
-ON ar.id = al.artist_id
-GROUP BY ar.name, ar.description, ar.thumbnail
-''')
-
-artist_detail = query('''
-SELECT * FROM artists
-LEFT JOIN albums
-ON artists.id = albums.artist_id
-''')
+        SELECT artists.id, artists.name, artists.description, artists.thumbnail, GROUP_CONCAT(albums.title, ',  ') AS ALBUM
+        FROM artists, albums
+        WHERE artists.id= albums.artist_id
+        GROUP BY artists.name, artists.description, artists.thumbnail;
+    ''')
 
 
 @app.get('/')
 def index():
+
     return render_template('index.html', artists=artists)
 
 
-@app.get('/details/<int:uid>')
-# get a dynamic parameter 'uid'. (always a string)
-def details(uid):
-    artists = query('''
-        SELECT artists.*, albums.*, albums.thumbnail AS album_thumbnail, COUNT(songs.id) AS Total_song FROM artists
+""" @app.get('/details/<int:detailid>')
+# get a dynamic parameter 'removeid'. (always a string)
+def details(detailid):
+
+    for artist in artists:
+        # runnig the loop method  to get all the elements
+        if artist['id'] == detailid:
+            # if randomly generated 'movie id' matches with the 'form movie id',then it removes
+            # remove elements(movie) from the list with remove() mehtod
+            return render_template('details.html', artist=artist)
+
+    # keep user on the same page
+ """
+
+""" @app.get('/info')
+def info(id):
+    db_albums = query('''
+        SELECT * FROM artists
         LEFT JOIN albums
         ON artists.id= albums.artist_id
-        JOIN songs
-        ON albums.id = songs.album_id
-        WHERE artists.id= :id
-        GROUP BY artists.name, albums.title''', {
-        'id': uid
+        WHERE artists.id= :id''', {
+        'id': int(id)
+    })
 
+    artists = {}
+
+    for row in db_albums:
+        print(row['id'], row['name'], row['description'])
+
+        id = row['id']
+
+# create row if not exists
+        if artists.get(id) == None:
+            artists[id] = {
+                'name': row['name'],
+                'description': row['description'],
+                'albums': []
+            }
+# add album to artist (if there is a album)
+        if row['title'] != None:
+            artists[id]['albums'].append({
+                'title': row['title'],
+                'description': row['description'],
+                'year_released': row['year_released']
+            })
+
+            return render_template('info.html', artist=db_albums)
+ """
+
+
+@app.get('/details/<int:uid>')
+# get a dynamic parameter 'removeid'. (always a string)
+def details(uid):
+    artists = query('''
+        SELECT artists.*, albums.*,albums.id AS album_id,  albums.thumbnail AS album_thumbnail FROM artists
+        LEFT JOIN albums
+        ON artists.id= albums.artist_id
+        WHERE artists.id= :id''', {
+        'id': uid
     })
 
     artists_info = {}
@@ -64,14 +100,59 @@ def details(uid):
 # add album to artist (if there is a album)
         if row['title'] != None:
             artists_info[id]['albums'].append({
+                # add album id
+                'id': row['id'],
                 'title': row['title'],
                 'year_released': row['year_released'],
                 'thumbnail': row.get('album_thumbnail')
-
             })
 
     artists = list(artists_info.values())
     return render_template('details.html', artist=artists[0])
 
-# to run the flask app
-app.run(debug=True)
+
+@app.get('/info/<int:aid>')
+# get a dynamic parameter 'removeid'. (always a string)
+def album_info(id):
+    albums = query('''
+        SELECT albums.*, songs.*
+        FROM albums
+        LEFT JOIN songs
+        ON albums.id= songs.album_id
+        WHERE albums.id= :id''', {
+        'id': id
+    })
+
+    albums_info = {}
+
+    for row in albums:
+        row = dict(row)
+
+        print(row['name'])
+
+        id = row['id']
+
+# create row if not exists
+        if albums_info.get(id) == None:
+            albums_info[id] = {
+                'title': row['title'],
+                'description': row['description'],
+                'year_released': row['year_released'],
+                'thumbnail': row.get('album_thumbnail'),
+                'songs ': []
+            }
+
+# add album to artist (if there is a album)
+        if row['title'] != None:
+            albums_info[id]['songs'].append({
+                'title': row['title'],
+                'year_released': row['year_released'],
+                'thumbnail': row.get('album_thumbnail')
+            })
+
+    albums = list(albums_info.values())
+    return render_template('info.html', album=albums[0])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
